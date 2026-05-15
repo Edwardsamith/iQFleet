bb7d21baa2bbb197b04a369a9378bf852301b67c
Eduard Ramirez <ramirezedward535@gmail.com>
2026-05-14 22:26:51 -0500

    feat: Clean Unused

--- src/main/java/Console/Main.java
+++ src/main/java/Console/Main.java
@@ -8,26 +8,10 @@ import Application.Mediator;
 import Application.Result.Result;
 import Application.Result.Unit;
 import Domain.Repositories.ExampleRepository;
-import Infrastructure.Repositories.FileExampleRepository;
 
 public class Main {
     public static void main(String[] args) {
 
-        Mediator mediator = new Mediator();
-        ExampleRepository repo = new FileExampleRepository();
-        mediator.registerHandler(CreateExampleCommand.class, new CreateExampleCommandHandler(repo));
-        mediator.registerHandler(DeleteExampleCommand.class, new DeleteExampleCommandHandler(repo));
-
-
-        Result<Unit> resulCreate = mediator.send(new CreateExampleCommand("Eduardo", "Ramirez"));
-
-        if(!resulCreate.isSuccess()){
-            for (String error : resulCreate.getErrors()){
-                System.out.println(error);
-            }
-        }else {
-            System.out.println("Guardado");
-        }
 
 
     }
