57ba26d37f8b0eab43eb942dd1fe9e44db3e0088
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-05-30 15:04:07 -0500

    feat: create register user command

---
+++ src/main/java/Application/User/Register/RegisterUserCommand.java
@@ -0,0 +1,18 @@
+package Application.User.Register;
+
+import Application.Abstractions.ICommand;
+import Domain.Enums.Role;
+import Domain.Enums.IdentificationType;
+
+public record RegisterUserCommand(
+        String firstName,
+        String lastName,
+        IdentificationType identificationType,
+        String identificationNumber,
+        String email,
+        String phone,
+        String username,
+        String password,
+        Role role
+) implements ICommand<RegisterUserResponse> {
+}
