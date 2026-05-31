48e1c702aa46ae98d65432063118c3794e133aaf
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-05-30 22:59:02 -0500

    feat: add update user response

---
+++ src/main/java/Application/Features/Users/Update/UpdateUserCommand.java
@@ -0,0 +1,16 @@
+package Application.Features.Users.Update;
+
+import Application.Abstractions.ICommand;
+import Domain.Enums.Role;
+
+import java.util.UUID;
+
+public record UpdateUserCommand(
+        UUID userId,
+        String firstName,
+        String lastName,
+        String phone,
+        String email,
+        Role role
+) implements ICommand<UpdateUserResponse> {
+}
