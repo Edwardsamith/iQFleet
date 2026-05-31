58ecc64c7f74bf2cdf27b8fe83ac01196800264f
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-05-30 21:37:43 -0500

    feat: implement activate user feature

---
+++ src/main/java/Application/Features/Users/Activate/ActivateUserCommandHandler.java
@@ -0,0 +1,48 @@
+package Application.Features.Users.Activate;
+
+import Application.Abstractions.IRequestHandler;
+import Application.Result.Result;
+import Domain.Enums.UserStatus;
+import Domain.Repositories.UserRepository;
+
+public class ActivateUserCommandHandler
+        implements IRequestHandler<
+        ActivateUserCommand,
+        String> {
+
+    private final UserRepository repository;
+
+    public ActivateUserCommandHandler(
+            UserRepository repository
+    ) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Result<String> handle(
+            ActivateUserCommand command
+    ) {
+
+        var optionalUser =
+                repository.findById(
+                        command.userId()
+                );
+
+        if (optionalUser.isEmpty()) {
+
+            return Result.Failure(
+                    "Usuario no encontrado"
+            );
+        }
+
+        var user = optionalUser.get();
+
+        user.setStatus(UserStatus.ACTIVE);
+
+        repository.save(user);
+
+        return Result.Success(
+                "Usuario activado correctamente"
+        );
+    }
+}
