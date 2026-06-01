32fa3d3c4ddd00bb508aa2dbdb00468cf2f43307
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-05-31 22:10:11 -0500

    feat: add attributes export report command

--- src/main/java/Application/Features/Reports/Operational/GetOperationalReportQueryHandler.java
+++ src/main/java/Application/Features/Reports/Operational/GetOperationalReportQueryHandler.java
@@ -6,6 +6,6 @@ import Application.Result.Result;
 public class GetOperationalReportQueryHandler implements IRequestHandler<GetOperationalReportQuery, GetOperationalReportResponse> {
     @Override
     public Result<GetOperationalReportResponse>handle(GetOperationalReportQuery query){
-        return Result.Success(new GetOperationalReportResponse(0,0));
+        return Result.Success(new GetOperationalReportResponse(0,0, 0));
     }
 }
