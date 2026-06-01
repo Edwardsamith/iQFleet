74fd2dd8d68f8bd63c3968bfd8c816e90602fc7e
Alejandro Arias Cala <ariascalaalejandro@gmail.com>
2026-05-31 22:05:45 -0500

    feat: correction documentary report response

--- src/main/java/Application/Features/Reports/Documentary/GetDocumentaryReportResponse.java
+++ src/main/java/Application/Features/Reports/Documentary/GetDocumentaryReportResponse.java
@@ -1,8 +1,8 @@
 package Application.Features.Reports.Documentary;
 
 public record GetDocumentaryReportResponse(
-        int vigente,
-        int vencidos,
-        int proximosAVencer
+        int vigentes,
+        int proximosAVencer,
+        int vencidos
 ) {
 }
